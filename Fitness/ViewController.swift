a97403d8a4d803ceb9d2788a99261451b8dd74e3
田磊 <tianlei9@hotmail.com>
2015-12-08 16:48:45 +0800

    Initial Commit

---
+++ Fitness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fitness
+//
+//  Created by 田磊 on 15/12/8.
+//  Copyright © 2015年 田磊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
