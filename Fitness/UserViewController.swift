2e9bbbbbe3a11893d3b8a7e9f1576b0fbda722c4
田磊 <tianlei9@hotmail.com>
2015-12-11 11:43:24 +0800

    完成用户注册界面

--- Fitness/UserViewController.swift
+++ Fitness/UserViewController.swift
@@ -35,6 +35,30 @@ class UserViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: {
         NSLog("返回按钮点击")
         })
+        
+        
+    }
+    @IBAction func onFindPassword(sender: AnyObject) {
+        
+//        let incoming = sender as! UIButton
+//        var titlename:String = ""
+//        if incoming.titleLabel == "登录"     {
+//            titlename = "登录过程"
+//        }
+//        switch incoming.restorationIdentifier {
+//            case "登录":
+//                titlename = "登录过程"
+//            
+//        }
+        let titlename:String = String(sender.restorationIdentifier)
+        //NSLog(titlename)
+        
+        let alertPass:UIAlertController = UIAlertController(title: "找回密码", message: "进入\(titlename)界面", preferredStyle: UIAlertControllerStyle.Alert)
+        alertPass.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Cancel, handler: nil))
+        
+        self.presentViewController(alertPass, animated: true, completion: nil)
+        
+        
     }
 
 }
